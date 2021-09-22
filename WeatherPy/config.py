7f87593b35f30ae2623c8f1dff119d22b5ac970d
nedamzd <87897023+nedamzd@users.noreply.github.com>
2021-09-22 13:25:40 -0400

    Update config.py

--- WeatherPy/config.py
+++ WeatherPy/config.py
@@ -1,6 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "15be02d9c9474d81645f876ed8b22658"
+weather_api_key = "API"
 
 # Google API Key
-API_KEY = "AIzaSyAzoTNvr_Hjuc9ihatU8mYF6l8NIxhPg-0"
+API_KEY = "G_API"
 
